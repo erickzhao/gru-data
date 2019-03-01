815115b0bbdbf38085087257a83344480f75cfe1
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-03-01 03:10:34 -0500

    feat: Add email field

--- user/user.js
+++ user/user.js
@@ -9,8 +9,9 @@ for (let i = 0; i < 100; i++) {
   const first = faker.name.firstName();
   const last = faker.name.lastName();
   data.push({
-    username: faker.internet.userName(first),
+    username: faker.internet.userName(first,last),
     password: faker.internet.password(),
+    email: faker.internet.email(first,last),
     creation_date: faker.date.recent(1000),
     name: `${first} ${last}`
   });
