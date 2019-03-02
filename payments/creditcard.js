61ff2c63c9b20b14871ffca00daeeaf02ba5abb2
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-03-02 02:15:06 -0500

    feat: Add cc

---
+++ payments/creditcard.js
@@ -0,0 +1,22 @@
+const faker = require("faker");
+const json2csv = require("json2csv").parse;
+const getUsers = require('../user/user').getUsers
+const fs = require("fs");
+
+faker.seed(1337);
+
+const data = [];
+const users = getUsers();
+
+for (let i = 0; i < 100; i++) {
+  data.push({
+    date_added: faker.date.recent(10),
+    is_primary: false,
+    username: users[i].username,
+    public_key: faker.finance.creditCardNumber()
+  });
+}
+
+const csv = json2csv(data);
+
+fs.writeFileSync("payments/creditcard.csv", csv);
